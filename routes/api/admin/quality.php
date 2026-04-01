600e893a049210c68be2ca00d978062b2d753dec
suraj@fastranking.tech <FastRanking@123>
2026-04-01 13:54:34 +0100

    Update the folder structure of consultation module

--- routes/api/admin/quality.php
+++ routes/api/admin/quality.php
@@ -10,8 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['jwt.auth'])->prefix('quality')->name('quality.')->group(function () {
     require __DIR__ . '/quality/quality.php';
-});
-
-Route::middleware(['jwt.auth'])->prefix('quality-questions')->name('quality-questions.')->group(function () {
-    require __DIR__ . '/quality/quality.php';
+    require __DIR__ . '/quality/quality-questions.php';
+    require __DIR__ . '/quality/quality-data-submission.php';
+    require __DIR__ . '/quality/quality-audit.php';
 });
