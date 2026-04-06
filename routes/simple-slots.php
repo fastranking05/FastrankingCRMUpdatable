9008014e48dae794f9b3010b60766772bb491066
fastranking05 <161586668+fastranking05@users.noreply.github.com>
2026-04-06 15:40:08 +0100

    Delete routes directory

--- routes/simple-slots.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\Appointment\SimpleTimeSlotController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/api/simple-slots', [SimpleTimeSlotController::class, 'getAvailableSlots']);
