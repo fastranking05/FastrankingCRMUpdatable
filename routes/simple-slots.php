c0e91395e9e69c803aef4b282f4882c031a55b2a
suraj@fastranking.tech <FastRanking@123>
2026-04-07 14:33:23 +0100

    Add api of time slot blocking

--- routes/simple-slots.php
+++ routes/simple-slots.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\Api\Appointment\SimpleTimeSlotController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/api/simple-slots', [SimpleTimeSlotController::class, 'getAvailableSlots']);
+Route::post('/api/simple-slots/block', [SimpleTimeSlotController::class, 'blockSlot']);
+Route::post('/api/simple-slots/release', [SimpleTimeSlotController::class, 'releaseSlot']);
